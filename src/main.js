096a44acbbbf80127ee7924c2b147e3e11e9d111
Adam Franzen <adamfranzen@gmail.com>
2016-08-08 17:35:56 -0600

    Completed centering map on new location and calculating distance

--- src/main.js
+++ src/main.js
@@ -1,56 +1,62 @@
+var bcycleLocationLat;
+var bcycleLocationLon;
+
 // Your code here!
 $(function () {
   console.log("sanity check");
   getBStatus();
   getBStationInfo();
-  // geoFindMe();
+  geoFindMe();
 });
 
-  function getBStatus() {
-    $.ajax ({
-      url: 'https://gbfs.bcycle.com/bcycle_denver/station_status.json',
-      method: 'GET'
-    }).success(function (bStatus){
-      // console.log(bStatus);
-      // console.log(bStatus.data.stations);
-      for (var i = 0; i < bStatus.data.stations.length; i++) {
-        var stationID = (bStatus.data.stations[i].station_id);
+function getBStatus() {
+  $.ajax ({
+    url: 'https://gbfs.bcycle.com/bcycle_denver/station_status.json',
+    method: 'GET'
+  }).success(function (bStatus){
+    // console.log(bStatus);
+    // console.log(bStatus.data.stations);
+    for (var i = 0; i < bStatus.data.stations.length; i++) {
+      var stationID = (bStatus.data.stations[i].station_id);
 
-        var numBikesAvail = (bStatus.data.stations[i].num_bikes_available);
+      var numBikesAvail = (bStatus.data.stations[i].num_bikes_available);
 
-        var numDocksAvail = (bStatus.data.stations[i].num_docks_available);
+      var numDocksAvail = (bStatus.data.stations[i].num_docks_available);
 
-        // $("#select").append('<option>' + titles + '</option>');
-        //img
-      }
-    });
-  }
+      // $("#select").append('<option>' + titles + '</option>');
+      //img
+    }
+  });
+}
 
-  function getBStationInfo() {
-    $.ajax ({
-      url: 'https://gbfs.bcycle.com/bcycle_denver/station_information.json',
-      method: 'GET'
-    }).success(function (bStationInfo){
-      console.log(bStationInfo.data.stations);
-      for (var i = 0; i < bStationInfo.data.stations.length; i++) {
-        var address = (bStationInfo.data.stations[i].address);
+function getBStationInfo() {
+  $.ajax ({
+    url: 'https://gbfs.bcycle.com/bcycle_denver/station_information.json',
+    method: 'GET'
+  }).success(function (bStationInfo){
+    console.log(bStationInfo.data.stations);
+    for (var i = 0; i < bStationInfo.data.stations.length; i++) {
+      var address = (bStationInfo.data.stations[i].address);
 
-        var latB = (bStationInfo.data.stations[i].lat);
+      var latB = (bStationInfo.data.stations[i].lat);
 
-        var lonB = (bStationInfo.data.stations[i].lon);
+      var lonB = (bStationInfo.data.stations[i].lon);
+      bcycleLocationLat = latB;
+      bcycleLocationLon = lonB;
 
-        var stationIdInfo = (bStationInfo.data.stations[i].station_id);
+      var stationIdInfo = (bStationInfo.data.stations[i].station_id);
 
-        var numDocksAvail = (bStationInfo.data.stations[i].num_docks_available);
-      }
-    });
-  }
+      var numDocksAvail = (bStationInfo.data.stations[i].num_docks_available);
+    }
+  });
+}
 
-  function initMap(lat, lng, latB, lonB) {
 // Passing in lat and long perameters from geoFindMe
-  var myLatLng = {lat: lat, lng: lng};
+  function initMap(lat, lng) {
+// Passing in lat and long perameters from geoFindMe
+  var myLatLng = new google.maps.LatLng(lat, lng);
 
-  var bLatLong = {lat: 39.73078, lng: -104.97279};
+  var bLatLong = new google.maps.LatLng(bcycleLocationLat, bcycleLocationLon);
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
@@ -68,6 +74,7 @@ $(function () {
     map: map,
     title: 'B-cycle station'
   });
+  console.log('miles apart: ' + (((google.maps.geometry.spherical.computeDistanceBetween(myLatLng, bLatLong))*0.000621371).toFixed(2)));
 }
 
 // Retrieve user location
@@ -96,11 +103,11 @@ function geoFindMe() {
     // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
     //
     // output.appendChild(img);
-  };
+  }
 
   function error() {
     output.innerHTML = "Unable to retrieve your location";
-  };
+  }
 
   // output.innerHTML = "<p>Locating…</p>";
 
