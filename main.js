587340c9437b6f2bd03ff3526c4a98174d6c0efe
Adam Franzen <adamfranzen@gmail.com>
2016-08-12 09:49:34 -0600

    finalized for presentation

--- main.js
+++ main.js
@@ -1,101 +1,208 @@
-var bcycleLocationLat;
+// global
+var stations;
+var distances = [];
+var myLatLng;
+var nearestLatLon;
+var map;
 
-var bcycleLocationLon;
-
-// Your code here!
-$(function () {
-  console.log("sanity check");
-  getBStatus();
-  getBStationInfo();
-  geoFindMe();
+// jquery
+$(document).ready(function() {
+  $('.progress').animate({ width: '100%' }, 4000);
 });
 
+// promises.all returns an array with all data from each AJAX call
+
+Promise.all([getBStatus(), getBInfo()])
+  .then(mergeResponses)
+  .then(addGoogleMapsScript);
+
+function mergeResponses (responses) {
+
+  var statusResponse = responses[0];
+  var locationResponse = responses[1];
+
+  // creating a new object with status information
+
+  var stationsPathStatus = statusResponse.data.stations;
+  var combinedStationsObj = {};
+
+  for (var i = 0; i < stationsPathStatus.length; i++) {
+    var numBikesAvailKey = Object.keys(stationsPathStatus[i])[1];
+    var numStationsAvailKey = Object.keys(stationsPathStatus[i])[2];
+    var isRentingKey = Object.keys(stationsPathStatus[i])[4];
+    var isReturningKey = Object.keys(stationsPathStatus[i])[5];
+    combinedStationsObj[stationsPathStatus[i].station_id] = {
+      numBikesAvailKey: stationsPathStatus[i].num_bikes_available,
+      numStationsAvailKey: stationsPathStatus[i].num_docks_available,
+      isRentingKey: stationsPathStatus[i].is_renting,
+      isReturningKey: stationsPathStatus[i].is_returning
+    };
+  }
+
+  // adding info to the already created station object
+
+  var stationsFromInfo = locationResponse.data.stations;
+
+  for (var j = 0; j < stationsFromInfo.length; j++) {
+
+    var key = stationsFromInfo[j].station_id;
+    var lonVal = stationsFromInfo[j].lon;
+    var latVal = stationsFromInfo[j].lat;
+    var nameVal = stationsFromInfo[j].name;
+
+    combinedStationsObj[key].lat = latVal;
+    combinedStationsObj[key].lon = lonVal;
+    combinedStationsObj[key].name = nameVal;
+
+  }
+
+  stations = combinedStationsObj;
+
+}
+
 function getBStatus() {
-  $.ajax ({
-    url: 'https://gbfs.bcycle.com/bcycle_denver/station_status.json',
-    method: 'GET'
-  }).success(function (bStatus){
-    // console.log(bStatus);
-    // console.log(bStatus.data.stations);
-    for (var i = 0; i < bStatus.data.stations.length; i++) {
-      var stationID = (bStatus.data.stations[i].station_id);
-
-      var numBikesAvail = (bStatus.data.stations[i].num_bikes_available);
-
-      var numDocksAvail = (bStatus.data.stations[i].num_docks_available);
-
-      // $("#select").append('<option>' + titles + '</option>');
-      //img
-    }
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: 'https://gbfs.bcycle.com/bcycle_denver/station_status.json',
+      method: 'GET'
+    }).done(function (bStatusAJAXReturn) {
+      resolve(bStatusAJAXReturn);
+    }).fail(function(err) {
+      reject(err);
+    });
   });
 }
 
-function getBStationInfo() {
-  $.ajax ({
-    url: 'https://gbfs.bcycle.com/bcycle_denver/station_information.json',
-    method: 'GET'
-  }).success(function (bStationInfo){
-    console.log(bStationInfo.data.stations);
-    for (var i = 0; i < bStationInfo.data.stations.length; i++) {
-      var address = (bStationInfo.data.stations[i].address);
-
-      var latB = (bStationInfo.data.stations[i].lat);
-
-      var lonB = (bStationInfo.data.stations[i].lon);
-
-      bcycleLocationLat = latB;
-
-      bcycleLocationLon = lonB;
-
-      var stationIdInfo = (bStationInfo.data.stations[i].station_id);
-
-      var numDocksAvail = (bStationInfo.data.stations[i].num_docks_available);
-
-      // var marker = new google.maps.Marker({
-      //   position: myLatLng,
-      //   map: map,
-      //   title: 'Hello World!'
-
-      // });
-    }
+function getBInfo() {
+  return new Promise(function(resolve, reject) {
+    $.ajax ({
+      url: 'https://gbfs.bcycle.com/bcycle_denver/station_information.json',
+      method: 'GET'
+    }).done(function (bStationInfoAJAXReturn) {
+      resolve(bStationInfoAJAXReturn);
+    }).fail(function(err) {
+      reject(err);
+    });
   });
 }
-console.log(bcycleLocationLat);
 
-// Passing in lat and long perameters from geoFindMe
-  function initMap(lat, lng) {
+function addGoogleMapsScript () {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.src  = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDzaOBIjmRJeiSfhiXhPC4Wo4syHsQG_hc&callback=geoFindMe&libraries=geometry';
+  $('head').append(s);
+}
 
-  var myLatLng = new google.maps.LatLng(lat, lng);
+// Passing in lat and lon perameters from geoFindMe
 
-  var bLatLong = new google.maps.LatLng(bcycleLocationLat, bcycleLocationLon);
+function initMap (lat, lng) {
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
+  myLatLng = new google.maps.LatLng(lat, lng);
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
     center: myLatLng
   });
+
+  // instead of a for loop, use map
+  // var markers = stations.map(...);
+  // markers.forEach(... )
+
+  for (var station in stations) {
+    var latitude = (stations[station].lat);
+    var lon = (stations[station].lon);
+    var name = (stations[station].name);
+    var numBikesAvail =  (stations[station].numBikesAvilKey);
+    var coordinates = new google.maps.LatLng(latitude, lon);
+    mapMarker(map, coordinates, name);
+  }
 
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
-    title: 'Hello World!'
+    title: 'Current Location',
+    icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
   });
 
-  var marker2 = new google.maps.Marker({
-    position: bLatLong,
-    map: map,
-    title: 'B-cycle station'
+  // marker.setMap(map);
+  //
+  var infowindow = new google.maps.InfoWindow({
+    content:'Your current location'
   });
-  console.log('miles apart: ' + (((google.maps.geometry.spherical.computeDistanceBetween(myLatLng, bLatLong))*0.000621371).toFixed(2)));
+
+  infowindow.open(map,marker);
+
+  // return the marker from mapMarker
+
+  function mapMarker (map, coordinates, name) {
+    new google.maps.Marker({
+      position: coordinates,
+      map: map,
+      title: name,
+      icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+    });
+    var myLatLng = new google.maps.LatLng(lat, lng);
+    var distanceArray = distanceAway(coordinates, myLatLng, name, numBikesAvail);
+  }
+
+  function distanceAway(stationLoc, userLocation, name, numBikesAvail) {
+
+    var distance = parseFloat((google.maps.geometry.spherical.computeDistanceBetween(
+      stationLoc, userLocation) * 0.000621371).toFixed(2));
+
+    distances.push({
+      name: name,
+      distance: distance,
+      stationLatLon: stationLoc,
+      num_bikes_avail: numBikesAvail
+    });
+  }
+  detClosest(distances);
+
+}
+
+function detClosest(distancesArr) {
+  var index = 0;
+  var value = 100;
+
+  for (var i = 0; i < distancesArr.length; i++) {
+    if (distancesArr[i].distance < value) {
+      value = distancesArr[i].distance;
+      index = i;
+    }
+  }
+
+  var nearStationName = distancesArr[index].name;
+  var nearestDistance = value;
+  nearestLatLon = distancesArr[index].stationLatLon;
+
+  var pathTo = [myLatLng, nearestLatLon];
+  console.log(pathTo);
+
+  var flightPath = new google.maps.Polyline({
+    path:pathTo,
+    strokeColor:'#BB2034',
+    strokeOpacity:0.65,
+    strokeWeight:8
+  });
+
+  flightPath.setMap(map);
+  // var nearMile = value;
+  $('.station').prepend("<div class='col-md-6 text-center'><p> Distance (Miles): </p><p><span class='bold'>" + nearestDistance + "</span></p></div>");
+  $('.station').prepend("<div class='col-md-6 text-center'><p> Nearest Station: </p><p class='bold text-center'>" + nearStationName + "</p></div>");
+  $('.progress').remove();
+
 }
 
 // Retrieve user location
 
 function geoFindMe() {
 
-  var output = document.getElementById("out");
+  console.log('stations', stations);
 
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+  var output = document.getElementById('out');
+
+  if (!navigator.geolocation) {
+    output.innerHTML = '<p>Geolocation is not supported by your browser</p>';
     return;
   }
 
@@ -107,17 +214,10 @@ function geoFindMe() {
     console.log(longitude);
 
     initMap(latitude, longitude);
-
-    // output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-    //
-    // var img = new Image();
-    // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-    //
-    // output.appendChild(img);
   }
 
   function error() {
-    output.innerHTML = "Unable to retrieve your location";
+    output.innerHTML = 'Unable to retrieve your location';
   }
 
   // output.innerHTML = "<p>Locating…</p>";
